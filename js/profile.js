07f290d8c5a561fdee921841125ee10deadeb6e1
Alexander Garcia <alexnlt9@gmail.com>
2019-07-18 11:41:12 -0700

    Tab selection works

--- js/profile.js
+++ js/profile.js
@@ -4,3 +4,12 @@ $(document).ready(function () {
     });
     $('.sidenav').sidenav('open');
 });
+
+var tab = document.getElementsByClassName('parent-tab');
+for (var i = 0; i < tab.length; i++) {
+    tab[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
