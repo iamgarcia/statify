cd8640c1b42fcb89c51bbda69756f011c46ae002
Alexander Garcia <alexnlt9@gmail.com>
2019-07-18 13:00:13 -0700

    Almost done

--- js/profile.js
+++ js/profile.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
 
 var tab = document.getElementsByClassName('parent-tab');
 for (var i = 0; i < tab.length; i++) {
-    tab[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
+    tab[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
     });
-  }
+}
