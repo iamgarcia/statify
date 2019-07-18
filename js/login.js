d9feea5c9cced44623f64e372c30747a3c057ca0
Alex <me@alexandermgarcia.com>
2019-07-17 23:32:58 -0700

    Sidenav getting there

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
+$(document).ready(function(){
+  $('.sidenav').sidenav({
+    draggable: true
+  });
 });
---
+++ js/profile.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav({
+        edge: 'left'
+    });
+    $('.sidenav').sidenav('open');
+  });
