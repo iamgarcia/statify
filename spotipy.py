a8837e71ab3ba6cbef7e3f36302b1f12084bd18c
mutian00 <38056403+mutian00@users.noreply.github.com>
2019-07-31 01:09:02 -0400

    spotipy.py grabs more info

--- spotipy.py
+++ spotipy.py
@@ -1,10 +1,24 @@
-import os
-import sys
-import json
-import secret
-
 import spotipy
 from spotipy import util
 
-token = util.prompt_for_user_token('', 'user-top-read', client_id=secret.CLIENT_ID, client_secret=secret.CLIENT_SECRET, redirect_uri=secret.REDIRECT_URI)
-print(token)
+token = util.prompt_for_user_token('jawe;ofji', 'user-top-read', client_id='d1043c275ddc4f64a60b2438db8ef839', client_secret='fe8389b1944e44199785a6607fc68da4', redirect_uri='https://new-statify-app.appspot.com/')
+artists = []
+songs = []
+
+# {song tit: , artist:, album tit:, song time: }
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    top_artists = sp.current_user_top_artists(limit=50, time_range='short_term')
+    top_tracks = sp.current_user_top_tracks(limit=50, time_range='short_term')
+    print('favorite artists')
+    for item in top_artists['items']:
+        print(item['name'])
+    print('top tracks')
+    for item in top_tracks['items']:
+        song_length = sp.track(item['uri'][-22:])['duration_ms']
+        song_length = round(song_length/60000, 2)
+        songs = {'name': item['name'], 'artists': item['artists'][0]['name'], 'album_title': item['album']['name'], 'song_length': song_length}
+        print(songs)
+else:
+    print("can't get token for user")
