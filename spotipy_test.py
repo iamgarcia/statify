01fedc4de24031fb8caab4fa1871986ef93732d8
mutian00 <38056403+mutian00@users.noreply.github.com>
2019-07-31 18:26:18 -0400

    quick python updates

--- spotipy_test.py
+++ spotipy_test.py
@@ -1,6 +1,15 @@
 import spotipy
 from spotipy import util
 
+def get_songs(token):
+    sp = spotipy.Spotify(auth=token)
+    top_tracks = sp.current_user_top_tracks(limit=50, time_range='short_term')
+    for item in top_tracks['items']:
+        song_length = sp.track(item['uri'][-22:])['duration_ms']
+        song_length = round(song_length/60000, 2)
+        songs = {'name': item['name'], 'artists': item['artists'][0]['name'], 'album_title': item['album']['name'], 'song_length': song_length}
+    return songs
+
 token = util.prompt_for_user_token('jawe;ofji', 'user-top-read', client_id='d1043c275ddc4f64a60b2438db8ef839', client_secret='fe8389b1944e44199785a6607fc68da4', redirect_uri='https://new-statify-app.appspot.com/')
 artists = []
 songs = []
